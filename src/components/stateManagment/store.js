afd5e3b9b70beacd2e31347bae94b86f7ceace75
Sarah Moradi <moradisarah2001@gamil.com>
2022-12-13 15:29:45 +0330

    added redux-persist with configs in store file

--- src/components/stateManagment/store.js
+++ src/components/stateManagment/store.js
@@ -1,5 +1,17 @@
-import { createStore } from "redux";
-import reducres from "./reducres";
+import { persistReducer, persistStore } from 'redux-persist'
 
-const store = createStore(reducres);
-export default store;
+import { createStore } from 'redux'
+import reducres from './reducres'
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+const persistedReducer = persistReducer(persistConfig, reducres)
+
+export const store = createStore(persistReducer)
+export const persistor = createStore(store)
+
+// const store = createStore(reducres)
+// export default store
